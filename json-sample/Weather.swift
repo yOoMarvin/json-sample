ea4ba63810e416f5453cbd272609e3f9c579a5d8
yOoMarvin <marvin.messenzehl@gmail.com>
2017-05-21 19:58:31 +0200

    Add data fetch and call in controller

--- json-sample/Weather.swift
+++ json-sample/Weather.swift
@@ -29,4 +29,40 @@ struct Weather {
         self.temperature = temperature
     }
     
+    
+    static let basePath = "https://api.darksky.net/forecast/2d5a31bf6ea8b43e8f91ea88264c3e32/"
+    
+    static func forecast(withLocation location:String, completion: @escaping ([Weather]) -> ()) {
+        
+        let url = basePath + location
+        let request = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data: Data?, response:URLResponse?, error:Error?) in
+            
+            var forecastArray:[Weather] = []
+            
+            if let data = data {
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        if let dailyForecasts = json["daily"] as? [String:Any] {
+                            if let dailyData = dailyForecasts["data"] as? [[String:Any]]{
+                                for dataPoint in dailyData {
+                                    if let weatherObject = try? Weather(json: dataPoint) {
+                                        forecastArray.append(weatherObject)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
 }
